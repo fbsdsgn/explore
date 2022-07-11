51bb8b8b81ee33f030d7f7848ac06204d419f103
Felipe B Souza <fbsdsgn@gmail.com>
2022-07-11 12:04:38 -0300

    Adding stitches file and configuration

---
+++ src/styles/config/stitches.config.ts
@@ -0,0 +1,231 @@
+import { createStitches } from "@stitches/react";
+import type * as Stitches from "@stitches/react";
+export type { VariantProps } from "@stitches/react";
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+  reset,
+} = createStitches({
+  theme: {
+    colors: {},
+    sizes: {
+      0: "4px",
+      1: "8px",
+      2: "16px",
+      3: "24px",
+      4: "32px",
+      5: "40px",
+      6: "48px",
+      7: "56px",
+      8: "64px",
+      9: "72px",
+      10: "80px",
+      11: "88px",
+      12: "96px",
+      13: "104px",
+      14: "112px",
+      15: "120px",
+      16: "128px",
+      17: "136px",
+      18: "144px",
+      19: "152px",
+      20: "160px",
+    },
+
+    space: {
+      1: "8px",
+      2: "16px",
+      3: "24px",
+      4: "32px",
+      5: "40px",
+      6: "48px",
+      7: "56px",
+      8: "64px",
+      9: "72px",
+      10: "80px",
+      11: "88px",
+      12: "96px",
+      13: "104px",
+      14: "112px",
+      15: "120px",
+      16: "128px",
+      17: "136px",
+      18: "144px",
+      19: "152px",
+      20: "160px",
+    },
+    fontSizes: {
+      1: "8px",
+      2: "16px",
+      3: "24px",
+      4: "32px",
+      5: "40px",
+      6: "48px",
+      7: "56px",
+      8: "64px",
+      9: "72px",
+      10: "80px",
+      11: "88px",
+      12: "96px",
+      13: "104px",
+      14: "112px",
+      15: "120px",
+      16: "128px",
+      17: "136px",
+      18: "144px",
+      19: "152px",
+      20: "160px",
+    },
+  },
+  media: {
+    bp1: "(max-width: 520px)",
+    bp2: "(max-width: 900px)",
+    bp3: "(max-width: 1200px)",
+    bp4: "(max-width: 1800px)",
+    motion: "(prefers-reduced-motion)",
+    hover: "(any-hover: hover)",
+    dark: "(prefers-color-scheme: dark)",
+    light: "(prefers-color-scheme: light)",
+  },
+  utils: {
+    p: (value: Stitches.PropertyValue<"padding">) => ({
+      padding: value,
+    }),
+    pt: (value: Stitches.PropertyValue<"paddingTop">) => ({
+      paddingTop: value,
+    }),
+    pr: (value: Stitches.PropertyValue<"paddingRight">) => ({
+      paddingRight: value,
+    }),
+    pb: (value: Stitches.PropertyValue<"paddingBottom">) => ({
+      paddingBottom: value,
+    }),
+    pl: (value: Stitches.PropertyValue<"paddingLeft">) => ({
+      paddingLeft: value,
+    }),
+    px: (value: Stitches.PropertyValue<"paddingLeft">) => ({
+      paddingLeft: value,
+      paddingRight: value,
+    }),
+    py: (value: Stitches.PropertyValue<"paddingTop">) => ({
+      paddingTop: value,
+      paddingBottom: value,
+    }),
+
+    m: (value: Stitches.PropertyValue<"margin">) => ({
+      margin: value,
+    }),
+    mt: (value: Stitches.PropertyValue<"marginTop">) => ({
+      marginTop: value,
+    }),
+    mr: (value: Stitches.PropertyValue<"marginRight">) => ({
+      marginRight: value,
+    }),
+    mb: (value: Stitches.PropertyValue<"marginBottom">) => ({
+      marginBottom: value,
+    }),
+    ml: (value: Stitches.PropertyValue<"marginLeft">) => ({
+      marginLeft: value,
+    }),
+    mx: (value: Stitches.PropertyValue<"marginLeft">) => ({
+      marginLeft: value,
+      marginRight: value,
+    }),
+    my: (value: Stitches.PropertyValue<"marginTop">) => ({
+      marginTop: value,
+      marginBottom: value,
+    }),
+
+    ta: (value: Stitches.PropertyValue<"textAlign">) => ({ textAlign: value }),
+
+    fd: (value: Stitches.PropertyValue<"flexDirection">) => ({
+      flexDirection: value,
+    }),
+    fw: (value: Stitches.PropertyValue<"flexWrap">) => ({ flexWrap: value }),
+
+    ai: (value: Stitches.PropertyValue<"alignItems">) => ({
+      alignItems: value,
+    }),
+    ac: (value: Stitches.PropertyValue<"alignContent">) => ({
+      alignContent: value,
+    }),
+    jc: (value: Stitches.PropertyValue<"justifyContent">) => ({
+      justifyContent: value,
+    }),
+    as: (value: Stitches.PropertyValue<"alignSelf">) => ({ alignSelf: value }),
+    fg: (value: Stitches.PropertyValue<"flexGrow">) => ({ flexGrow: value }),
+    fs: (value: Stitches.PropertyValue<"flexShrink">) => ({
+      flexShrink: value,
+    }),
+    fb: (value: Stitches.PropertyValue<"flexBasis">) => ({ flexBasis: value }),
+
+    bc: (value: Stitches.PropertyValue<"backgroundColor">) => ({
+      backgroundColor: value,
+    }),
+
+    br: (value: Stitches.PropertyValue<"borderRadius">) => ({
+      borderRadius: value,
+    }),
+    btrr: (value: Stitches.PropertyValue<"borderTopRightRadius">) => ({
+      borderTopRightRadius: value,
+    }),
+    bbrr: (value: Stitches.PropertyValue<"borderBottomRightRadius">) => ({
+      borderBottomRightRadius: value,
+    }),
+    bblr: (value: Stitches.PropertyValue<"borderBottomLeftRadius">) => ({
+      borderBottomLeftRadius: value,
+    }),
+    btlr: (value: Stitches.PropertyValue<"borderTopLeftRadius">) => ({
+      borderTopLeftRadius: value,
+    }),
+
+    bs: (value: Stitches.PropertyValue<"boxShadow">) => ({ boxShadow: value }),
+
+    lh: (value: Stitches.PropertyValue<"lineHeight">) => ({
+      lineHeight: value,
+    }),
+
+    ox: (value: Stitches.PropertyValue<"overflowX">) => ({ overflowX: value }),
+    oy: (value: Stitches.PropertyValue<"overflowY">) => ({ overflowY: value }),
+
+    pe: (value: Stitches.PropertyValue<"pointerEvents">) => ({
+      pointerEvents: value,
+    }),
+    us: (value: Stitches.PropertyValue<"userSelect">) => ({
+      WebkitUserSelect: value,
+      userSelect: value,
+    }),
+
+    userSelect: (value: Stitches.PropertyValue<"userSelect">) => ({
+      WebkitUserSelect: value,
+      userSelect: value,
+    }),
+
+    size: (value: Stitches.PropertyValue<"width">) => ({
+      width: value,
+      height: value,
+    }),
+
+    appearance: (value: Stitches.PropertyValue<"appearance">) => ({
+      WebkitAppearance: value,
+      appearance: value,
+    }),
+    backgroundClip: (value: Stitches.PropertyValue<"backgroundClip">) => ({
+      WebkitBackgroundClip: value,
+      backgroundClip: value,
+    }),
+  },
+});
+
+export type CSS = Stitches.CSS<typeof config>;
+
+export const darkTheme = createTheme("dark-theme", {
+  colors: {},
+});
