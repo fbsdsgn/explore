0192b755141904628da89173b2f4f9740a89876d
Felipe B Souza <fbsdsgn@gmail.com>
2022-07-15 13:45:22 -0300

    Adding component styles

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,57 @@
+import { styled } from "../../styles/config/stitches.config";
+
+export const Container = styled("header", {
+  border: "1px solid red",
+  display: "flex",
+  height: 88,
+  jc: "center",
+  ai: "center",
+  position: "fixed",
+  width: "100vw",
+  top: 0
+});
+
+export const Navbar = styled("nav", {
+  width: "100%",
+  maxWidth: "1216px",
+  display: "flex",
+  jc: "space-between",
+  ai: "center",
+});
+
+export const Logo = styled("a", {
+  fontSize: "$3",
+  p: "$1 $2",
+  color: "#fff",
+});
+
+export const NavList = styled("ul", {
+  width: "100%",
+  maxWidth: "460px",
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+export const Item = styled("li", {
+  fontSize: "$2",
+  color: "#fff",
+});
+
+export const Link = styled("a", {
+  p: "$1 $2",
+  border: "1px solid red",
+  cursor: "pointer",
+
+  variants: {
+    color: {
+      blue: {
+        color: "#2A97FC",
+      },
+    },
+  },
+});
+
+export const Wrapper = styled("div", {
+  display: "flex",
+  gap: "$3"
+});
